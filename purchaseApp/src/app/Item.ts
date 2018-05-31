2ecf0ffc290a6b0d74b811b135c80d50070d0eb6
Vitalii Isaenko <lookyisaenko@gmail.com>
2018-05-31 23:30:26 +0200

    Extract Item as a module

---
+++ purchaseApp/src/app/Item.ts
@@ -0,0 +1,14 @@
+class Item {
+    purchase: string;
+    done: boolean;
+    price: number;
+
+    constructor(purchase: string, price: number) {
+
+        this.purchase = purchase;
+        this.price = price;
+        this.done = false;
+    }
+}
+
+export {Item}
--- purchaseApp/src/app/app.component.ts
+++ purchaseApp/src/app/app.component.ts
@@ -1,42 +1,40 @@
 import { Component } from '@angular/core';
-     
-class Item{
-    purchase: string;
-    done: boolean;
-    price: number;
-     
-    constructor(purchase: string, price: number) {
-  
-        this.purchase = purchase;
-        this.price = price;
-        this.done = false;
-    }
-}
- 
+import {Item} from './Item';
+
 @Component({
     selector: 'purchase-app',
     templateUrl: '../../app.html',
-    styles: [`h2, p {color:#333;}`]    
+    styles: [`h2, p {color:#333;}`]
 })
-export class AppComponent { 
-    items: Item[] = 
-    [
-        { purchase: "Bread", done: false, price: 15.9 },
-        { purchase: "Butter", done: false, price: 60 },
-        { purchase: "Potato", done: true, price: 22.6 },
-        { purchase: "Cheese", done: false, price:310 }
-    ];
+export class AppComponent {
+    items: Item[] =
+        [
+            { purchase: "Bread", done: false, price: 15.9 },
+            { purchase: "Butter", done: false, price: 60 },
+            { purchase: "Potato", done: true, price: 22.6 },
+            { purchase: "Cheese", done: false, price: 310 }
+        ];
     addItem(text: string, price: number): void {
-         
-        if(text==null || text.trim()=="" || price==null)
+
+        if (text == null || text.trim() == "" || price == null)
             return;
         this.items.push(new Item(text, price));
     };
     name = "Mark";
     userAge = 122;
 
-    clicks:number = 0;
-    onChanged(increased:any){
-        increased==true?this.clicks++:this.clicks--;
+    clicks: number = 0;
+    onChanged(increased: any) {
+        increased == true ? this.clicks++ : this.clicks--;
+    }
+
+    constructor() { this.log(`constructor`); }
+
+    ngOnInit() { this.log(`onInit`); }
+
+    ngOnDestroy() { this.log(`onDestroy`); }
+
+    private log(msg: string) {
+        console.log(msg);
     }
 }
